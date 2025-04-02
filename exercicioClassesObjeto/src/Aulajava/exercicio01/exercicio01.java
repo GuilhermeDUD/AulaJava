f0c7b940c01a253059722d99437e415faa8b9f08
Guilherme Doudement <163214700+GuilhermeDUD@users.noreply.github.com>
2025-04-01 21:12:18 -0300

    Aula01/04/2025 - Atividades

---
+++ exercicioClassesObjeto/src/Aulajava/exercicio01/exercicio01.java
@@ -0,0 +1,13 @@
+package Aulajava.exercicio01;
+
+
+public class exercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
